e1b2d30ad4c1a40b6fdd46570f7cb1195884be18
Deals4gamer <mohithchowdary1982@gmail.com>
2026-01-21 15:31:15 +0530

    Launcher.ps1

--- Launcher.ps1
+++ Launcher.ps1
@@ -92,22 +92,41 @@ Pause
 
 # -------------------- REDUCED ASCII ART --------------------
 Write-Host ""
-Write-Host "         ..:::^^^^:::.      " -ForegroundColor Cyan
-Write-Host "    .^~7?YYYYYYYYYYYY5YYJ7!^.   " -ForegroundColor Cyan
-Write-Host "  ~:  .^7J555YJ?!~7?Y55PP55YJJ7~. " -ForegroundColor Cyan
-Write-Host " ~JPY^ :7YP5YJY??JY5PGGB####BBP555G57^" -ForegroundColor Cyan
-Write-Host "  JGP777?Y5PJ7Y55?YB###&BGGG5J77?Y5G57" -ForegroundColor Cyan
-Write-Host "   ~Y5??G55GJJY5JY&#B#&GG#BGGP5PB#&BP" -ForegroundColor Cyan
-Write-Host "      :::..757JP55YJJJ5P?PBG#@B#&@B##&&" -ForegroundColor Cyan
-Write-Host "        .7PPJ7?G57G&#PPPPPG?G&&@@@&&5G" -ForegroundColor Cyan
-Write-Host "       ~YGPJ!7JJ?Y#&G!~?GBG!5~#@@@&G?YB" -ForegroundColor Cyan
-Write-Host "      !JJYYY?J5J5775#PJ!JBGP7~#&@&YJJJJ?" -ForegroundColor Cyan
-Write-Host "        ?B&#B5?!~~?JGBGP75Y~G7P#&?7J7YY?" -ForegroundColor Cyan
-Write-Host "          ~B&&&&&57?YJ?5GGPJY~5G?G#@Y7:?@" -ForegroundColor Cyan
-Write-Host "             :!YP55B&&&&PJJ5P!~7J5Y~J#BG&" -ForegroundColor Cyan
-Write-Host "                ^5GGGY???Y5Y5Y!?J5PPGPY?!~" -ForegroundColor Cyan
-Write-Host "                     :~?55Y??P5YPJ!!?5YJ5YJ?" -ForegroundColor Cyan
-Write-Host "                         .^B#G55Y?7?YP5PG77" -ForegroundColor Cyan
-Write-Host "                            ~^.:B@GJ&#BGPPPGG5" -ForegroundColor Cyan
-Write-Host "                               .^~7JYPGGBBGP5Y?7~:." -ForegroundColor Cyan
+
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠟⠁⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣿⣿⣿⣧⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⣷⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⠩⢹⡙⠁⢸⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠃⢹⡇⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣤⣼⣶⣿⡆⠀⢸⣿⣾⣿⠻⣿⠷⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⠀⠀⢿⡄⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⡄⠀⢈⣿⡄⣰⣴⡀⢀⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠇⠀⠀⠸⣧⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣷⣽⣿⣿⣬⣉⣡⠞⢻⣿⣿⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⣿⡀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⠿⠻⣿⠛⣿⣇⠀⠀⠀⣼⣿⠈⠉⠑⠲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⠀⠀⠀⠀⢸⡇" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢞⠁⠀⠀⠹⣦⣻⣿⣆⠀⣠⣿⣿⠀⣠⢀⡀⠈⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⠀⠀⣸⡇" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠀⠀⣰⡀⠀⠈⢻⣿⣏⠙⠋⣸⣿⣮⡏⠀⢳⠀⢸⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠀⠀⠀⠀⢠⣿⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⢶⣤⣠⡏⠱⣄⣠⡀⠹⣿⣄⠘⣿⡿⢿⣰⣦⡏⠀⣈⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⠀⠀⢠⣾⠃⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞⠀⠀⢉⡿⢳⠀⠈⠚⠓⠒⢻⣿⣷⣿⠉⠉⠉⢹⠳⡾⠛⠁⠸⣄⣀⣀⡀⠀⠀⠀⠀⢸⠀⠀⠀⢠⣿⠇⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⣴⡋⠀⠀⢧⡀⠀⠀⠀⠘⢿⣿⡿⠷⣄⡀⡎⠀⠉⠢⣄⡀⠀⠀⠀⠉⠓⠦⢤⣀⣟⣀⣀⣤⣼⡟⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⢦⡀⠀⠀⠉⠓⠠⠬⢧⡤⠞⠛⢻⣿⡆⠀⠀⢈⣿⣧⣄⠀⠀⠀⠉⠒⠢⠄⠀⣀⣀⣀⣀⣿⣍⠀⣽⣿⣷⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠠⠠⠤⠤⠤⢿⣤⣤⣤⣴⣿⣤⣤⣴⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠀⠈⠉⣿⡟⠁⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⢠⠤⠒⠂⢤⣤⡀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⠯⢏⣳⣾⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠇⠀⠀⣼⡿⠀⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⡼⠭⠭⢭⣿⣗⣿⣆⠀⠀⠀⠀⢀⣿⣿⣿⣿⡶⣾⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⣸⣿⠁⠀⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⣟⣒⣒⠠⡿⢭⣿⣿⣷⣄⡀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠀⢠⣿⠇⠀⠀⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⢸⣗⣒⣒⣹⡿⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠻⣉⣴⣿⣿⢭⡒⢭⣱⡀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀⢸⡏⠀⠀⠀⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⢹⡿⠭⠽⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⢟⣩⡠⢚⣿⣿⣿⣿⡶⠚⣩⠟⣇⡀⠀⠀⠀⠀⠀⠀⡜⠀⠀⣿⠇⠀⠀⠀⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⢈⣯⢏⣙⣿⡻⣿⣿⣿⣿⣿⡿⠿⡭⠞⣡⠟⠋⣠⣾⣿⣿⣿⣿⢭⣭⡥⠔⢋⡗⠀⠀⠀⠀⠀⢰⡇⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠘⡿⢒⣺⡯⠿⠛⠛⠋⡹⠐⢋⡠⠊⣡⠴⣏⡽⠿⣿⣿⣿⠏⣀⡠⢤⣶⡫⣧⠀⠀⠀⠀⠀⡼⠀⠀⠀⣿⡄⠀⠀⠀⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠋⠉⠀⠀⠀⠀⢀⣰⠥⠒⠋⡴⣻⣄⣾⡉⢳⣤⠾⣿⣟⣋⡡⠔⠋⠸⠊⣸⠀⠀⠀⠀⢠⡇⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠁⠀⠀⠀⡠⠟⢁⡸⢛⠗⠛⣷⣴⣿⡟⠷⣤⣄⠀⡠⠜⢩⣧⠀⠀⠀⢸⠀⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⠀⠀⣠⢞⣹⡤⠤⠄⡊⠤⠒⠋⢀⢾⠀⡠⠛⣶⣿⣧⡀⠈⠑⢥⡀⡠⣿⢿⠀⠀⠀⡇⠀⠀⠀⠀⢸⡟⠀⠀⠀⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠀⢀⡸⠃⠀⠀⠀⠀⠈⠀⠀⢀⣤⡟⠀⠙⢳⡔⣿⠿⣈⠛⠷⣦⣤⡍⣚⡵⠛⣧⠀⣸⠇⠀⠀⠀⢀⣿⠃⠀⠀⠀⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⢰⠟⢆⡀⠀⠀⠀⢀⡠⠄⠒⣩⠞⠀⢀⠔⠛⣿⣿⣦⡀⠉⠒⠤⣈⠉⡁⠤⠚⠹⡄⡟⠀⠀⠀⠀⣾⠏⠀⠀⠀⠀⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⢰⡀⠀⠙⠒⠒⠒⠁⠀⡠⠊⡏⠀⠀⠸⠤⣼⡿⠉⣷⢌⡓⠢⠤⣀⡀⠀⠀⠀⠀⣷⠇⠀⠀⠀⣼⡏⠀⡀⢀⣠⣀⡤⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⢰⡷⡀⠀⠀⠀⣀⣴⣊⣠⠞⠀⠀⠀⠀⠀⠿⠁⠀⠈⢶⣾⠷⢦⡤⡉⠁⠐⠂⠈⣽⣧⡀⢀⣼⠟⠀⠀⡿⣿⠟⠁⠀⠀⠀⠀" -ForegroundColor Cyan
+Write-Host "⠀⠀⠀⠀⠈⢿⣟⠒⢚⡭⢀⣠⡴⠋⠀⠀⣀⣀⣤⣤⣄⣀⣀⣄⣀⣹⣦⣄⠙⠚⠷⢶⣶⣺⣿⡿⣷⣾⣯⣤⣤⣴⣷⣷⣤⣶⣿⣿⣿⣷" -ForegroundColor Cyan
+Write-Host "⠀⣠⣤⣴⣦⣶⣿⣭⣽⣾⣿⣿⣷⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣶⣦⣴⣿⣾⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣿⣟⣿" -ForegroundColor Cyan
+Write-Host "⣾⣿⣿⣿⣿⣿⣿⣿⡟⠛⢻⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⠟⠻⣿⣿⣿⣿⣿⣿⣿⣿⣮⣿⣟⣿" -ForegroundColor Cyan
+Write-Host "⣰⣿⣿⣿⣿⣿⣿⣿⠁⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣧⣄⡀⠀⢙⣿⣿⣿⣿⣿⣿⣿⡿⢽⣻" -ForegroundColor Cyan
+Write-Host "⣤⠿⣴⡿⣻⣿⣿⣿⣷⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⠿⠿⠿⠿⠿⠿⢿⣿⣿⣯⣙⣒⣺⣻⡿⠿⠿⠿⢦⣬⣿⡿⠛⠛⠋⠉⠉⢉⣍⠹⣏" -ForegroundColor Cyan
+Write-Host "⡽⠒⠛⠛⠋⠉⠉⠉⠙⠋⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣈⠿⡿⣂" -ForegroundColor Cyan
+
 Write-Host ""
